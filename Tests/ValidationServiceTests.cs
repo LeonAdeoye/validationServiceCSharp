0a3c8db88e29e8e4925089fb8dcccdf020ecb142
Leon Adeoye <leon.adeoye@gmail.com>
2021-12-25 21:39:24 +0800

    added more test cases

--- Tests/ValidationServiceTests.cs
+++ Tests/ValidationServiceTests.cs
@@ -23,4 +23,70 @@ public class ValidationServiceTests
         var result = validationService.ValidateRow(0, new string[] {}, new ValidationConfiguration[] { });
         Assert.AreEqual(0, result.Count());
     }
+
+    [Test]
+    public void ValidateRow_MixedRows_ReturnsOneError()
+    {
+        var result = validationService.ValidateRow(0, new string[] { "HORATIO", "HARPER" }, new ValidationConfiguration[] 
+            { 
+                new ValidationConfiguration()
+                {
+                    Id = 0, 
+                    Type = "string", 
+                    StringFormat = "UPPERCASE"
+                },
+                new ValidationConfiguration()
+                {
+                    Id = 1,
+                    Type = "string",
+                    StringFormat = "LOWERCASE"
+                }
+            });
+        Assert.AreEqual(1, result.Count());
+        Assert.AreEqual("Row: 0 and column: 1 has a validation error: Cannot validate value: HARPER as a lowercase string.", result.First());
+    }
+
+    [Test]
+    public void ValidateRow_ValidRows_ReturnsNoErrors()
+    {
+        var result = validationService.ValidateRow(0, new string[] { "HORATIO", "harper" }, new ValidationConfiguration[]
+        {
+            new ValidationConfiguration()
+            {
+                Id = 0,
+                Type = "string",
+                StringFormat = "UPPERCASE"
+            },
+            new ValidationConfiguration()
+            {
+                Id = 1,
+                Type = "string",
+                StringFormat = "LOWERCASE"
+            }
+        });
+        Assert.AreEqual(0, result.Count());
+    }
+
+    [Test]
+    public void ValidateRow_ValidRow_ReturnsNoErrors()
+    {
+        var result = validationService.ValidateRow(0, new string[] { "horatio", "HARPER" }, new ValidationConfiguration[]
+        {
+            new ValidationConfiguration()
+            {
+                Id = 0,
+                Type = "string",
+                StringFormat = "UPPERCASE"
+            },
+            new ValidationConfiguration()
+            {
+                Id = 1,
+                Type = "string",
+                StringFormat = "LOWERCASE"
+            }
+        });
+        Assert.AreEqual(2, result.Count());
+        Assert.AreEqual("Row: 0 and column: 0 has a validation error: Cannot validate value: horatio as a uppercase string.", result.First());
+        Assert.AreEqual("Row: 0 and column: 1 has a validation error: Cannot validate value: HARPER as a lowercase string.", result.Last());
+    }
 }
