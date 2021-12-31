cfec914b9d1a0fc97496eecccb27a56c0a11e6be
Leon Adeoye <leon.adeoye@gmail.com>
2021-12-31 17:25:29 +0800

    added logging to main program

--- Program.cs
+++ Program.cs
@@ -10,11 +10,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services via dependency injection.
 builder.Services.AddSingleton<IValidationService, ValidationService>();
 builder.Services.AddSingleton<IValidatorFactory, ValidatorFactory>();
 builder.Services.AddSingleton<EmptyValidator>();
 
 var app = builder.Build();
+
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+logger.LogInformation("Added all services via dependency injection.");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
